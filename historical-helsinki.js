b9ecd5e6153ef21ececa0edd521a230652b3f911
jaukia <jaukia@thinglink.com>
2013-06-17 09:28:45 +0300

    Added lens view and cleaned up code base

--- historical-helsinki.js
+++ historical-helsinki.js
@@ -1,120 +1,140 @@
-/*
- * Layer selectors
- */
-
-$("select").change(function() {
-    var value = this.options[this.selectedIndex].value;  
-    map.showHistoricalLayer(value);
-    var $mapDetails = $("#"+value+"-details");
-    $(".mapDetails div").hide();
-    $mapDetails.show();
-});
+/* Some parts of the code based on Old Oakland by M. Migurski, used with permission */
 
 $(document).ready(function() {
-    function selectBackgroundLayer(which) {
-        if(which==="road") {
-            map.showBaseLayer('Present Day (VEarth road)');
-        } else {
-            map.showBaseLayer('Present Day (VEarth aerial)');
+
+    // SETUP CORE FUNCTIONALITY
+    
+    var historicalLayers = [
+        {name:"1837: Map by C.W. Gyldén", id:"helsinki1837"},
+        {name:"1897: Map by Matkailija-Yhdistys", id:"helsinki1897"},
+        {name:"1943: Aerial photos", id:"helsinki1943"},
+        {name:"1968: Smith-Polvinen plan (II, M1)", id:"smithpolvinen-m1"}
+    ];
+    
+    var initialLayer = 1;
+                           
+    map = new HistoricalMap('map');
+    map.setCenter(60.166920, 24.936841, 13);
+
+    map.setSelectedLayerChangeListener(function(mapName) {
+        console.log("hiphei",mapName);
+        $(".mapDetails > div").hide();
+        $(".mapDetails #"+mapName+"-details").show();
+    });
+
+    var mapSelector = $("select");
+    
+    for(var i=0;i<historicalLayers.length;i++) {
+        var curLayer = historicalLayers[i];
+        map.addHistoricalLayer(curLayer.id);
+        
+        var optElem = $('<option value="'+curLayer.id+'">'+curLayer.name+'</option>');
+        
+        if(initialLayer===i) {
+            optElem.attr("selected","selected");
+            map.selectHistoricalLayer(curLayer.id);
         }
-        $(".backgroundMapLink").removeClass("active");
-        $(this).addClass("active");
+        
+        mapSelector.append(optElem);
     }
     
+    // SETUP LAYER SELECTOR
+    
+    $("select").change(function() {
+        var value = this.options[this.selectedIndex].value;  
+        map.selectHistoricalLayer(value);
+        /*var $mapDetails = $("#"+value+"-details");
+        $(".mapDetails div").hide();
+        $mapDetails.show();*/
+    });
+
+    // BACKGROUND LAYER SELECTOR
+    
     $("#roadMapLink").click(function() {
-        selectBackgroundLayer.call(this,"road");
+        if(!$(this).hasClass("active")) {
+            map.selectBaseLayer('bg-road');
+            $(".backgroundMapLink").toggleClass("active");
+        }
     });
     
     $("#aerialMapLink").click(function() {
-        selectBackgroundLayer.call(this,"aerial");
+        if(!$(this).hasClass("active")) {
+            map.selectBaseLayer('bg-aerial');
+            $(".backgroundMapLink").toggleClass("active");
+        }
     });
+    
+    // MODE SELECTOR
+    
+    $("#lensModeLink").click(function() {
+        if(!$(this).hasClass("active")) {
+            map.setMode('lens');
+            $("#slider").hide();
+            $(".modeLink").toggleClass("active");
+        }
+    });
+    
+    $("#transparencyModeLink").click(function() {
+        if(!$(this).hasClass("active")) {
+            map.setMode('transparency');
+            $("#slider").show();
+            $(".modeLink").toggleClass("active");
+        }
+    });
+    
+    // SLIDER SETUP
+    
+    $("#slider").noUiSlider({
+        range: [0, 255],
+        start: [200],
+        handles: 1,
+        step: 1,
+        slide: function(){
+            var value = $(this).val();
+            console.log("slide",value);
+            var roundValue = value/255.0;
+            map.setTransparency(roundValue);
+        }
+    });
+    
 });
 
-
-/*
- * Slider setup
- */
-
-function init_slider() {
-    var slider = YAHOO.widget.Slider.getHorizSlider("sliderbg", "sliderthumb", 0, 250, 0);
-    slider.getRealValue = function() {
-      return Math.round(this.getValue() * 0.005 * 100) / 100;
-    }
-    slider.setValue(250);
-    slider.subscribe("change", function(offsetFromStart) {
-        // FIXME: loop all layers, or do this only for the visible layers
-      map.layers['map1837'].setOpacity(slider.getRealValue());
-      map.layers['map1897'].setOpacity(slider.getRealValue());
-      map.layers['aerial1943'].setOpacity(slider.getRealValue());
-      map.layers['smithpolvinen'].setOpacity(slider.getRealValue());
-    });
-}
-
-/**
- * Based on:
- * http://teczno.com/old-oakland/OaklandLayers.js
- */
-
-/**
- * Given a set of coordinates for a tile, return a directory-style string.
- *
- * @param   x   Number  column
- * @param   y   Number  row
- * @param   z   Number  zoom
- *
- * @return  String  E.g. "0313131311132123" suitable for inclusion in a tile URL
- */
 function coordinateToString(x, y, z) {
     var a = [];
-    
     for(var i = 0; i < z; i += 1) {
         var r = (y & 1);
         var c = (x & 1);
-        
         a.unshift(c | (r << 1));
-        
         x = x >> 1;
         y = y >> 1;
     }
-    
     s = '';
-    
     for(var i = 0; i < a.length; i += 1) {
         s += a[i].toString();
     }
-    
     return s;
 }
 
 function getXYZ(layer, bounds) {
-
     var res = layer.map.getResolution();
     var x = Math.round ((bounds.left - layer.maxExtent.left) / (res * layer.tileSize.w));
     var y = Math.round ((layer.maxExtent.top - bounds.top) / (res * layer.tileSize.h));
     var z = layer.map.getZoom();
-    
     return [x, y, z];
 }
 
-/*
- * Returns a getURL() method for use in layer args.
- */
-function getGetURLMethod(bucket_id, file_ext) {
-    return function(bounds)
-    {
+function constructGetURLMethod(bucket_id, file_ext) {
+    return function(bounds) {
         var xyz = getXYZ(this, bounds);
         var x = xyz[0], y = xyz[1], z = xyz[2];
-        
         return bucket_id+'/'+z+'-r'+y+'-c'+x+'.'+file_ext;
     };
 }
 
-/**
- * Wrapper for map.
- */
-function HelsinkiMap(div) {
+// Map object implementation
 
-    /* The "correct" way to invoke a spherical mercator slippy grid map, per crshmidt's advice */
+function HistoricalMap(div) {
+
     var mapArgs = {
         maxExtent: new OpenLayers.Bounds(-20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892),
         numZoomLevels: 18,
@@ -127,97 +147,42 @@ function HelsinkiMap(div) {
     
     var layerAerialArgs = {
         buffer: 0,
-        getURL: function(bounds)
-        {
+        getURL: function(bounds) {
             var xyz = getXYZ(this, bounds);
             var x = xyz[0], y = xyz[1], z = xyz[2];
-            
             // predictable? server: 0, 1, 2, 3
             var s = (x + y + z) % 4;
-            
             return 'http://a'+s+'.ortho.tiles.virtualearth.net/tiles/a'+coordinateToString(x, y, z)+'.jpeg?g=90';
         },
         isBaseLayer: true
-    };
-
-    var layerVEarthArgs = {
-        buffer: 0,
-        getURL: function(bounds)
-        {
-            var xyz = getXYZ(this, bounds);
-            var x = xyz[0], y = xyz[1], z = xyz[2];
-            
-            // predictable? server: 0, 1, 2, 3
-            var s = (x + y + z) % 4;
-            
-            return 'http://r'+s+'.ortho.tiles.virtualearth.net/tiles/r'+coordinateToString(x, y, z)+'.png?g=90&shading=hill';
-        },
-        isBaseLayer: true
-    };
-
-    var layer1897Args = {
-        buffer: 0,
-        getURL: getGetURLMethod('tiles/helsinki1897', 'png'),
-        isBaseLayer: false,
-        visibility: true
-    };
-    
-    var layer1837Args = {
-        buffer: 0,
-        getURL: getGetURLMethod('tiles/helsinki1837', 'png'),
-        isBaseLayer: false,
-        visibility: false
-    };
-    
-    var layer1943Args = {
-        buffer: 0,
-        getURL: getGetURLMethod('tiles/helsinki1943', 'png'),
-        isBaseLayer: false,
-        visibility: false
-    };
-    
-    var layerSmithPolvinenArgs = {
-        buffer: 0,
-        getURL: getGetURLMethod('tiles/smithpolvinen-m1', 'png'),
-        isBaseLayer: false,
-        visibility: false
     };
     
     this.map = new OpenLayers.Map(div, mapArgs);
     
     this.layers = {};
-    this.addHistoricalLayer('map1837', layer1837Args);
-    this.addHistoricalLayer('map1897', layer1897Args);
-    this.addHistoricalLayer('aerial1943', layer1943Args);
-    this.addHistoricalLayer('smithpolvinen', layerSmithPolvinenArgs);
-    this.addOSMLayer('Present Day (VEarth road)');
-    this.addHistoricalLayer('Present Day (VEarth aerial)', layerAerialArgs);
+    
+    this.addOSMLayer('bg-road');
+    this.addTMSLayer('bg-aerial', layerAerialArgs);
 
     this.map.addControl(new OpenLayers.Control.LayerSwitcher());
     this.map.addControl(new OpenLayers.Control.Navigation());
-    this.map.addControl(new OpenLayers.Control.PanZoomBar() );
-    //this.map.addControl(new OpenLayers.Control.Permalink());
-    //this.map.addControl(new OpenLayers.Control.ScaleLine());
-
-    this.setCenter(60.166920, 24.936841, 13);
+    this.map.addControl(new OpenLayers.Control.PanZoomBar());
+    
+    this.selectedLayer = null;
+    this.mode = "lens";
+    this.transparency = 250;
+    this.selectedLayerChangeListener = null;
 }
 
-HelsinkiMap.prototype = {
+HistoricalMap.prototype = {
 
-   /**
-    * Project latitude and longitude before passing onto the map object.
-    *
-    * @param    lat     Number  latitude in degrees
-    * @param    lon     Number  longitude in degrees
-    * @param    zoom    Number  zoom level integer
-    */
     setCenter: function(lat, lon, zoom) {
         var lonlat = new OpenLayers.LonLat(lon, lat);
         lonlat.transform(this.map.displayProjection, this.map.getProjectionObject());
         this.map.setCenter(lonlat, zoom);
     },
     
-    addHistoricalLayer: function(name, args) {
+    addTMSLayer: function(name, args) {
         this.layers[name] = new OpenLayers.Layer.TMS(name, null, args);
         this.map.addLayer(this.layers[name]);
     },
@@ -227,21 +192,94 @@ HelsinkiMap.prototype = {
         this.map.addLayer(this.layers[name]);
     },
     
-    showBaseLayer: function(name) {
+    addHistoricalLayer: function(name) {
+        var args = {
+            buffer: 0,
+            getURL: constructGetURLMethod('tiles/'+name, 'png'),
+            isBaseLayer: false,
+            visibility: false
+        };
+        return this.addTMSLayer(name, args);
+    },
+    
+    selectBaseLayer: function(name) {
         this.map.setBaseLayer(this.layers[name]);
         return false;
     },
     
-    showHistoricalLayer: function(name) {
+    getSelectedLayer: function(name) {
+        return this.selectedLayer;
+    },
+    
+    setTransparency: function(value) {
+        this.transparency = value;
+        this.selectedLayer.setOpacity(value);
+    },
+    
+    selectHistoricalLayer: function(name) {
         for(var i in this.layers) {
             if(i==name) {
-                //this.layers[i].display(true);
                 this.layers[i].setVisibility(true);
+                this.selectedLayer = this.layers[i];
+                if(this.mode==="lens") {
+                    // init lens with the new layer
+                    this.setMode("lens");
+                } else if(this.mode==="transparency") {
+                    this.setTransparency(this.transparency);
+                }
+                this.selectedLayerChangeListener.call(null,name);
             } else if(!this.layers[i].isBaseLayer) {
-                //this.layers[i].display(false);
                 this.layers[i].setVisibility(false);
             }
         }
         return false;
+    },
+    
+    getOpenLayersMap: function() {
+        return this.map;
+    },
+    
+    setSelectedLayerChangeListener: function(listenerFunc) {
+        this.selectedLayerChangeListener = listenerFunc;
+    },
+    
+    setMode: function(mode) {
+    
+        if(mode==="transparency") {
+            
+            if(this.layerLens) {
+                // this does destroying of the layerLens automatically?
+                this.map.removeControl(this.layerLens);
+                this.layerLens = null;
+            }
+            
+            this.mode = "transparency";
+            this.selectedLayer.setVisibility(true);
+            
+        } else if(mode==="lens") {
+            this.mode = "lens";
+            
+            var curLayerName = this.selectedLayer.name;
+            // has to be a base layer for lens to work
+            var args = {
+                buffer: 0,
+                getURL: constructGetURLMethod('tiles/'+curLayerName, 'png'),
+                isBaseLayer: true,
+                visibility: false
+            };
+         
+            var lensLayer = new OpenLayers.Layer.TMS(curLayerName, null, args);
+            
+            if(this.layerLens) {
+                this.layerLens.setLayer(lensLayer);
+            } else {
+                this.layerLens = new OpenLayers.Control.LayerLens(lensLayer);
+                this.map.addControl(this.layerLens);
+            }
+            this.layerLens.update();
+            
+            this.selectedLayer.setVisibility(false);
+            
+        }
     }
 };
